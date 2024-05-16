4ffb118d3aa05836b1a2b208bd2a06546dd1f444
常全辉 <zhi28881407@163.com>
2024-05-16 14:48:28 +0800

    add update host component  and function component

--- src/mini-react.js
+++ src/mini-react.js
@@ -39,9 +39,71 @@ function render(element,container){
 
 }
 
-function workLoop(){
+function workLoop(deadline){
+    let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+        shouldYield = deadline.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop)
+}
+
+function performUnitOfWork(fiber){
+
+    const isFunctionComponent = fiber.type instanceof Function
+
+    if(isFunctionComponent){
+        updateFunctionComponent(fiber)
+    }else{
+        updateHostComponent(fiber)
+    }
+
+
+    if(fiber.child){
+        return fiber.child
+    }
+
+    let nextFiber = fiber
+
+    while (nextFiber){
+        if(nextFiber.sibling){
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.return
+    }
 
 }
+
+
+let wipFiber = null
+let stateHooksIndex = null
+
+function updateFunctionComponent(fiber){
+    wipFiber = fiber
+    stateHooksIndex = 0
+    wipFiber.stateHooks = []
+    wipFiber.effectHooks = []
+
+    const children = [fiber.type(fiber.props)]
+    reconcileChildren(fiber,children)
+
+}
+
+function updateHostComponent(fiber){
+    if(!fiber.dom){
+        fiber.dom = createDom(fiber)
+    }
+
+    reconcileChildren(fiber,fiber.props.children)
+}
+
+
+function reconcileChildren (){
+
+}
+
+
+
 
 
 
